bcc5c78919694a327afdc0598f230bb489cf8f91
gerard <gtorrents@lasalle.cat>
2024-03-21 19:07:19 +0100

    Arreglado error pitográfico

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Nuevo cmabio");
+        System.out.println("Nuevo cambio");
     }
 }
